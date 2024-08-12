bf27eae76b01fc12299e2c476811ef10068ec42e
anouar ben mbarek <benmbark.anouar@gmail.com>
2024-08-12 11:41:25 +0100

    Add JobController with CRUD operations for jobs

---
+++ app/Http/Controllers/Jobcontroller.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Job;
+use Illuminate\Http\Request;
+
+
+class JobController extends Controller
+{
+    public function index() {
+        $jobs = Job::with('employer')->latest()->simplePaginate(5);
+
+        return view('jobs.index',
+         [
+            'jobs'=> $jobs
+        ]);
+    }
+
+    public function show(Job $job) {
+        return view('jobs.show',['job' => $job]);
+    }
+
+    public function create() {
+        $request = new Request();
+        $request->replace([
+            'title' => request('title'),
+            'salary' => request('salary')
+        ]);
+
+        $request->validate([
+            'title' => 'required|min:3',
+            'salary' => 'required'
+        ]);
+
+            Job::create([
+                'title'=> request('title') ,
+                'salary' => request('salary'),
+                'employer_id'=> 3 // for employer id , it would be fetched from the authenticated employer later , after integrating the auth .
+            ]);
+
+            return redirect('/jobs');
+    }
+
+    public function edit(Job $job) {
+        return view('jobs.edit',['job' => $job]);
+    }
+
+    public function update(Job $job) {
+
+        $job->update([
+            'title'=>request('title'),
+            'salary'=>request('salary')
+        ]);
+
+        return redirect('job/' . $job->id);
+
+    }
+
+    public function destroy(Job $job) {
+        $job->delete();
+        return redirect('/jobs');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,91 +1,13 @@
 <?php
 
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
-use \Illuminate\Support\Arr;
-use App\Models\Job;
 
-Route::get('/', function () {
-    return view('home');
-});
-
-
-
-Route::get('/jobs', function () {
-    $jobs = job::with('employer')->latest()->simplePaginate(5);
-
-    return view('jobs.index',
-     [
-        'jobs'=> $jobs
-    ]);
-});
-
-Route::get('/job/create',function() {
-    return view('jobs.create');
-});
-
-Route::get('/contact', function () {
-    return view('contact');
-});
-
-
-Route::post('/jobs' , function(){
-
-$request = new Illuminate\Http\Request();
-$request->replace([
-    'title' => request('title'),
-    'salary' => request('salary')
-]);
-
-$request->validate([
-    'title' => 'required|min:3',
-    'salary' => 'required'
-]);
-
-    Job::create([
-        'title'=> request('title') ,
-        'salary' => request('salary'),
-        'employer_id'=> 3 // for employer id , it would be fetched from the authenticated employer later , after integrating the auth .
-    ]);
-
-    return redirect('/jobs');
-});
-
-Route::get('/job/{id}/edit', function ($id) {
-    $job = job::find($id);
-    return view('jobs.edit',['job' => $job]);
-});
-
-Route::patch('/job/{id}', function ($id) {
-    // steps : validation , Auth(check user id)
-    // $request = new Illuminate\Http\Request();
-    // $request->validate([
-    //     'title' => ['required','min:3'],
-    //     'salary' => ['required']
-    // ]);
-
-    // validation error to fix later
-
-    $job = Job::findOrFail($id);
-
-    $job->update([
-        'title'=>request('title'),
-        'salary'=>request('salary')
-    ]);
-
-    return redirect('job/' . $job->id);
-
-});
-
-Route::delete('/job/{id}', function ($id) {
-
-    // auth on hold until making breeze auth
-    Job::findOrFail($id)->delete();
-    return redirect('/jobs');
-
-});
-
-
-Route::get('/job/{id}', function ($id) {
-    $job = job::find($id);
-    return view('jobs.show',['job' => $job]);
-});
+Route::view('/', 'home');
+Route::view('/job/create', 'jobs.create');
+Route::get('/jobs', [JobController::class , 'index'] );
+Route::post('/jobs' ,[JobController::class , 'create']);
+Route::get('/job/{job}/edit', [JobController::class , 'edit']);
+Route::patch('/job/{job}',[JobController::class , 'update']);
+Route::delete('/job/{job}', [JobController::class , 'destroy']);
+Route::get('/job/{job}', [JobController::class , 'show']);
